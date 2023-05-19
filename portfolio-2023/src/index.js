ec9f9d974d11e012eedc65502de0c10362cc5cd4
minafletcher <fletcher.mi@husky.neu.edu>
2023-05-19 11:16:52 -0400

    add project routes

--- portfolio-2023/src/index.js
+++ portfolio-2023/src/index.js
@@ -1,15 +1,64 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-import Portfolio from './Portfolio.js'
-import './style.css'
+import Portfolio from "./Portfolio.js";
+import "./style.css";
+import Navbar from "./components/Navbar/Navbar.js";
 
-const root = createRoot(document.querySelector('#root'))
+import ScrollToTop from "./scrollToTop.js";
+import {
+  GlobalEnergy,
+  NYUnderground,
+  MakeSwitch,
+  LogoBook,
+  ClaymationMusic,
+  PersonalLogo,
+  PrincessSack,
+  SpaceJunk,
+  GhostTalent,
+  Slanted,
+  CleanKick,
+  Benvenuti,
+  TheraPro,
+  CGP,
+  Interventions,
+  GoGarden,
+} from "./project-pages/projectPages.js";
+
+const root = createRoot(document.querySelector("#root"));
 
 root.render(
-    <>
-    <BrowserRouter>
-        <Portfolio />
-    </BrowserRouter>
-    </>
-)
+  <>
+    <Router>
+      <ScrollToTop />
+
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Portfolio />}></Route>
+        {/* Web Development Project Routes */}
+        <Route path="work/gogarden" element={<GoGarden />} />
+        <Route path="work/interventions-amplify" element={<Interventions />} />
+        <Route path="work/coolgreenpower" element={<CGP />} />
+        <Route path="work/therapro" element={<TheraPro />} />
+        <Route path="work/benvenuti" element={<Benvenuti />} />
+        <Route path="work/clean-kick" element={<CleanKick />} />
+        {/* Animation Project Routes */}
+        <Route path="work/slanted" element={<Slanted />} />
+        <Route path="work/ghost-talent-show" element={<GhostTalent />} />
+        <Route path="work/space-junk" element={<SpaceJunk />} />
+        <Route path="work/princess-floursack" element={<PrincessSack />} />
+        <Route path="work/personal-logo" element={<PersonalLogo />} />
+        <Route
+          path="work/claymation-music-video"
+          element={<ClaymationMusic />}
+        />
+        {/* Design Project Routes */}
+        <Route path="work/make-the-switch" element={<MakeSwitch />} />
+        <Route path="work/new-york-underground" element={<NYUnderground />} />
+        <Route path="work/global-energy-usage" element={<GlobalEnergy />} />
+        <Route path="work/24-logos-and-typefaces" element={<LogoBook />} />
+        <Route path="*" element={<Link to="/" replace />} />
+      </Routes>
+    </Router>
+  </>
+);
